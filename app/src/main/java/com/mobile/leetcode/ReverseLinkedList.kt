5a086dc16f7aa82e914e3be7bf72fb20f9f807d2
sureshmaidaragi <maidaragi1919@gmail.com>
2022-12-03 13:05:35 +0530

    Reverse linkedlist without recursion

--- app/src/main/java/com/mobile/leetcode/ReverseLinkedList.kt
+++ app/src/main/java/com/mobile/leetcode/ReverseLinkedList.kt
@@ -1,6 +1,6 @@
 package com.mobile.leetcode
 
-import org.w3c.dom.Node
+import java.util.*
 
 /*
 * Given the head of a singly linked list, reverse the list, and return the reversed list.
@@ -19,6 +19,69 @@ Input: head = []
 Output: []
 
 * */
+
+internal class LList {
+
+
+    internal class Node(var data: Int) {
+        var next: Node? = null
+    }
+
+    fun reverseLList(head: Node?): Node? {
+        var node: Node? = head
+
+        var prev: Node? = null
+        var next: Node? = null
+        var current: Node? = head
+
+        while (current != null) {
+            next = current.next
+            current.next = prev
+            prev = current
+            current = next
+        }
+
+        node = prev
+        return node
+
+    }
+
+    fun printList(root: Node?) {
+
+        var node = root
+        while (node != null) {
+            print("${node.data}  ")
+            node = node.next
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+
+            var head: Node? = null
+
+            head = Node(5)
+            head.next = Node(10)
+            head.next!!.next = Node(20)
+            head.next!!.next!!.next = Node(30)
+            head.next!!.next!!.next!!.next = Node(40)
+
+            var ll = LList()
+            println("original list")
+            ll.printList(head)
+            println("")
+            var reversedNode = ll.reverseLList(head)
+            println("after reverse")
+            ll.printList(reversedNode)
+        }
+    }
+
+
+}
+
+
+
 
 
 
