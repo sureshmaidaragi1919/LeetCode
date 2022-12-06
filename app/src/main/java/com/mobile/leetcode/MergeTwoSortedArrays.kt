2f6925d65f381d94671c2cb8f959fea2586aef76
sureshmaidaragi <maidaragi1919@gmail.com>
2022-12-06 18:52:58 +0530

    MergeTwoSortedArrays.kt

---
+++ app/src/main/java/com/mobile/leetcode/MergeTwoSortedArrays.kt
@@ -0,0 +1,28 @@
+package com.mobile.leetcode
+
+import java.util.*
+
+/*
+* Time Complexity: O( nlog(n) + mlog(m) )
+*/
+fun mergeArrays(a: IntArray, b: IntArray) {
+    val mp: MutableMap<Int, Boolean> = TreeMap() //Using tree map because internally it sorts the items
+
+    for (element in a) {
+        mp[element] = true
+    }
+    for (element in b) {
+        mp[element] = true
+    }
+
+    for (entries in mp.entries) {
+        print(entries.key.toString() + " ")
+    }
+}
+
+fun main() {
+    val a = intArrayOf(1, 3, 5, 7)
+    val b = intArrayOf(2, 4, 6, 8)
+
+    mergeArrays(a, b)
+}
