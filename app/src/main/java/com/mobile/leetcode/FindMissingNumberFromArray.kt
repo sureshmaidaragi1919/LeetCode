d1e00eae08ae0f24d66acd5fb58ece9df3ea3bb7
sureshmaidaragi <maidaragi1919@gmail.com>
2023-08-13 19:05:18 +0530

    Find MissingNumberFromArray

---
+++ app/src/main/java/com/mobile/leetcode/FindMissingNumberFromArray.kt
@@ -0,0 +1,18 @@
+package com.mobile.leetcode
+
+fun findMissingNumbers(arr: IntArray, count: Int): Int {
+    var total: Int = (count + 1) * (count + 2) / 2
+    var i = 0
+    while (i < count) {
+        total -= arr[i]
+        i++
+    }
+    return total
+}
+
+
+ fun main( ) {
+    val arr = intArrayOf(1, 2, 4, 6, 3, 7, 8)
+     println(arr.size)
+    println(findMissingNumbers(arr, arr.size))
+}
