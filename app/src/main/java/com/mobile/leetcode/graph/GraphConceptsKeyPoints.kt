be8c8d047489bd3ff23a12f89cbbfad818db4f96
sureshmaidaragi <maidaragi1919@gmail.com>
2023-08-30 01:23:52 +0530

    Graph basic concepts

---
+++ app/src/main/java/com/mobile/leetcode/graph/GraphConceptsKeyPoints.kt
@@ -0,0 +1,37 @@
+package com.mobile.leetcode.graph
+
+
+/*
+
+
+1. Graph is a data structure contains data at nodes also called as vertex and this vertex attached by line called edge
+2. There can be directed or undirected graph
+3. Directed graph means there is direction given between two nodes/vertices
+4. Undirected graph means there wont be any predefined directions between nodes
+5. What is weighted graph ? A graph if it contains weight on every edge or any edges is called as weighted graph
+6. What are different ways to store a graph :a. Using Matrix (n+1 nodes X m+1 edges) it is also called as adjency matrix b. using list called as adjency list
+7. What is indegree and outdegree in graph?. It is applicable to only directed graph, hence if a graph node contains incoming edges are indegree of node if a graph contains outgoing eges are called outdegree of node
+8. What is a edge weight in a graph
+9. Undirected graph is always a cyclic graph if all nodes are connected.
+10. What is a path in a graph?: If there is continuoue edges between given nodes then it is called as path
+11. What is degree in a graph? :  it is number of nodes attached to it are called as degree of edge formula is D = 2*Edges
+12. If we can't uniformly move across all the edge's and nodes then it is non-cyclic graph
+13. What are connected components in a graph? : a graph can be collection of group of graph then it is called as connected components of graph,
+14 A graph can contain single node too.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+* */
