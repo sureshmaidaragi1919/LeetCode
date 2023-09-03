a69b85d44f797fe2542bbc008bc70a8785258d0a
sureshmaidaragi <maidaragi1919@gmail.com>
2023-09-03 11:24:26 +0530

    Min and Max TimeRequiredToPaintAllFence

---
+++ app/src/main/java/com/mobile/leetcode/matrix/MinTimeRequiredToPaintAllFencesWith3Colors.kt
@@ -0,0 +1,76 @@
+package com.mobile.leetcode.matrix
+
+
+/*
+
+Given an n fences and 3 colors, user can paint all the fence with different color with no two adjust fence have same color
+find out min time required to paint all fences
+
+The time required to paint each fence with certain color is represented by a nx3 time matrix called times
+
+
+Example : times = [[2,9,4],[20,7,15],[18,12,19]]
+        output = 27
+*
+* */
+
+private fun main() {
+
+    val n = 3
+    val colorsCount = 3
+    var input = mutableListOf(
+        mutableListOf(2, 9, 4), mutableListOf(20, 7, 15), mutableListOf(18, 12, 19)
+    )
+
+    minTimeRequiredToPaintAllFence(input, colorsCount)
+    maxTimeRequiredToPaintAllFence(input, colorsCount)
+
+}
+
+private fun minTimeRequiredToPaintAllFence(input: MutableList<MutableList<Int>>, colorsCount: Int) {
+    val outputMap = mutableMapOf<Int, Pair<Int, Int>>()
+    for (i in input.indices) {
+        var min = Int.MAX_VALUE
+        for (j in 0..<colorsCount) {
+            if (outputMap[i - 1]?.second != j && min >= input[i][j]) {
+                min = input[i][j]
+                outputMap.put(i, Pair(min, j))
+
+            }
+        }
+    }
+
+
+    var result = 0
+    for (item in outputMap.iterator()) {
+        result += item.value.first
+    }
+
+    println("Min time required to paint all fence's is = $result")
+
+}
+
+
+private fun maxTimeRequiredToPaintAllFence(input: MutableList<MutableList<Int>>, colorsCount: Int) {
+    val outputMap = mutableMapOf<Int, Pair<Int, Int>>()
+    for (i in input.indices) {
+        var max = Int.MIN_VALUE
+        for (j in 0..<colorsCount) {
+            if (outputMap[i - 1]?.second != j && max <= input[i][j]) {
+                max = input[i][j]
+                outputMap.put(i, Pair(max, j))
+
+            }
+        }
+    }
+
+
+    var result = 0
+    for (item in outputMap.iterator()) {
+        result += item.value.first
+    }
+
+    println("Max time required to paint all fence's is = $result")
+
+}
+
