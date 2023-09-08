077d1333c3b7a0b0021582258af32808ed691f69
sureshmaidaragi <maidaragi1919@gmail.com>
2023-09-08 16:07:31 +0530

    Rotate Array using optimal approaches

--- app/src/main/java/com/mobile/leetcode/array/RotateArray.kt
+++ app/src/main/java/com/mobile/leetcode/array/RotateArray.kt
@@ -13,9 +13,8 @@ package com.mobile.leetcode.array
 private fun main() {
 
     val input = arrayOf(1, 2, 3, 4, 5, 6, 7)
-    val k = 1
-
-    rotateArrayUsingBruteForce(input, k)
+    val k = 6
+    rotateArrayUsingSizeOfkthTempArray(input, k)
 }
 
 
@@ -38,4 +37,62 @@ private fun rotateArrayUsingBruteForce(input: Array<Int>, k: Int) {
     }
 
     println(input.toList())
+}
+
+
+/*
+Optimal  approach
+using temp array of size arr.size
+
+Time complexity O(n)
+Space complexity O(1)
+*/
+
+private fun rotateArrayUsingNaiveApproach(input: Array<Int>, k: Int) {
+
+    if (k >= input.size) return
+
+    var s = input.size
+    var output = Array(s, init = { -1 })
+    var tempCounter = 0
+
+    for (i in k..<s) {
+        output[tempCounter++] = input[i]
+    }
+    println(output.toList())
+
+    var shit = s - 1
+    for (i in k - 1 downTo 0) {
+        output[shit--] = input[i]
+    }
+
+    println(output.toList())
+
+}
+
+/*
+* Here used extra space of arr[kth] instead arr[input.size] at @param rotateArrayUsingNaiveApproach
+* */
+private fun rotateArrayUsingSizeOfkthTempArray(input: Array<Int>, k: Int) {
+
+    if (k >= input.size) return
+    val kthArray = Array(k, init = { 0 })
+
+    for (i in 0..<k) {
+        kthArray[i] = input[i]
+    }
+    println(kthArray.toList().toString())
+
+    var j = 0
+    for (i in k..<input.size) {
+        input[j++] = input[i]
+    }
+
+
+    for (i in kthArray.indices) {
+        input[j++] = kthArray[i]
+    }
+
+    println("${input.toList()}")
+
 }
