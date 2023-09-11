ceff443134d8840f823c22c6c9eead5149aa1e17
sureshmaidaragi <maidaragi1919@gmail.com>
2023-09-11 15:47:42 +0530

    Print path to a node in Binary Tree

--- app/src/main/java/com/mobile/leetcode/tree/LowestCommonAncestorFromBinaryTree.kt
+++ app/src/main/java/com/mobile/leetcode/tree/LowestCommonAncestorFromBinaryTree.kt
@@ -18,16 +18,16 @@ fun main() {
     root.left = Node(2)
     root.right = Node(3)
 
-    root.right!!.left = Node(8)
-    root.right!!.right = Node(9)
-    root.left!!.right = Node(5)
-    root.left!!.left = Node(4)
-    root.left!!.right!!.right = Node(7)
-    root.left!!.right!!.left = Node(6)
+    root.right?.left = Node(8)
+    root.right?.right = Node(9)
+    root.left?.right = Node(5)
+    root.left?.left = Node(4)
+    root.left?.right?.right = Node(7)
+    root.left?.right?.left = Node(6)
 
 
 
-    println("Found ${findLowestCommonAncestor(root, Node(2), Node(6))!!.data}")
+    println("Found ${findLowestCommonAncestor(root, Node(1), Node(1))?.data}")
 }
 
 fun findLowestCommonAncestor(root: Node?, p: Node, q: Node): Node? {
---
+++ app/src/main/java/com/mobile/leetcode/tree/PrintNodePathFromRoot.kt
@@ -0,0 +1,50 @@
+package com.mobile.leetcode.tree
+
+import java.util.Stack
+
+
+/*
+*                             1
+*                     2                3
+*                4        5          8   9
+*                       6   7
+*
+* find common ancestor in above binary tree LCA (4, 7) == 2
+*
+* */
+
+
+private fun main() {
+
+    val root = Node(1)
+    root.left = Node(2)
+    root.right = Node(3)
+
+    root.right?.left = Node(8)
+    root.right?.right = Node(9)
+    root.left?.right = Node(5)
+    root.left?.left = Node(4)
+    root.left?.right?.right = Node(7)
+    root.left?.right?.left = Node(6)
+
+    val stack: Stack<Any?> = Stack()
+
+    printPath(root, 7, stack)
+
+    println(stack.toList().toString())
+
+}
+
+private fun printPath(root: Node?, data: Int, stack: Stack<Any?>): Boolean {
+
+    if (root == null) return false
+
+    stack.add(root.data)
+
+    if (root.data==data) return true
+
+    if (printPath(root.left,data,stack) || printPath(root.right,data,stack)) return true
+
+    stack.pop()
+    return false
+}
