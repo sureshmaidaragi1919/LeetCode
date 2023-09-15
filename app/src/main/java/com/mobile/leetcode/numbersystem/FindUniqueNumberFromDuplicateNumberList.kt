ee4fa27dd02906ad6149c30571513316e99c7a3e
sureshmaidaragi <maidaragi1919@gmail.com>
2023-09-15 23:32:55 +0530

    Clean up

--- app/src/main/java/com/mobile/leetcode/numbersystem/FindUniqueNumberFromDuplicateNumberList.kt
+++ app/src/main/java/com/mobile/leetcode/numbersystem/FindUniqueNumberFromDuplicateNumberList.kt
@@ -4,15 +4,15 @@ private fun main() {
 
     val input = arrayOf(2, 3, 3, 4, 2, 6, 4)
 
-    findunique(input)
+    findUnique(input)
 }
 
-fun findunique(input: Array<Int>) {
+fun findUnique(input: Array<Int>) {
 
     var unique = 0;
     for (element in input) {
         unique = element.xor(unique)
     }
 
-    println(unique)
+    println("Result is  $unique")
 }
